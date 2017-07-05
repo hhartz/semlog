c257f87590bae66c677f5dd9bc5bb82b69d94a6f
Henrik Hartz <henrik@cutehacks.com>
2017-07-05 09:30:04 +0200

    feat: enable reading from file with `--file=/path`

--- semlog/main.swift
+++ semlog/main.swift
@@ -9,9 +9,39 @@
 
 import Foundation
 
-var standardInput = FileHandle.standardInput
-var inputData = FileHandle.standardInput.availableData
-var changes = String.init(data: inputData, encoding: .utf8) ?? ""
+func usage() {
+    let executable = (CommandLine.arguments[0] as NSString).lastPathComponent
+    print("usage: ")
+    print("pipe output from e.g. git to \(executable).")
+    print("or")
+    print("\(executable) --file=/path/to/file to format a text file")
+}
+
+let args = CommandLine.arguments
+var changes = ""
+
+if args.count > 1, args[1] == "--help" || args[1] == "-h" {
+    usage()
+    exit(0)
+} else if args.count > 2, let file = args[2].components(separatedBy: "=").last {
+    if let fileData = FileManager.default.contents(atPath: file),
+        let fileString = String.init(data: fileData, encoding: .utf8) {
+        changes = fileString
+    } else {
+        print("unable to open or read \"\(file)\"")
+        exit(0)
+    }
+} else {
+    print("interactive mode. Ctrl-C to end input")
+    var standardInput = FileHandle.standardInput
+    var inputData = FileHandle.standardInput.availableData
+    if let inputString = String.init(data: inputData, encoding: .utf8) {
+        changes = inputString
+    } else {
+        print("unable to get input")
+    }
+}
+
 
 let semlog = SemanticLog(with: changes)
 print(semlog.output)
